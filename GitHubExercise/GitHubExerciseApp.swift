a294f51136fa6669ef750f91cfea89d826d95131
Gustavo Monge <egnom.ai@gmail.com>
2024-08-28 14:52:59 -0600

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Gustavo Monge on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
