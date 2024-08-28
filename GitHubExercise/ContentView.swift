c0d167e93175d503c21a4cb567a3e9006c765632
Gustavo Monge <egnom.ai@gmail.com>
2024-08-28 15:16:43 -0600

    My personal first commit

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "i.circle")
+                .resizable()
+                .scaledToFit()
+
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundStyle(.red)
     }
 }
 
