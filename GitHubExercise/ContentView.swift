e5163016e3dc3759b484917d36517a1dfc1c1f56
Gustavo Monge <egnom.ai@gmail.com>
2024-08-28 18:39:58 -0600

    Version 2.0 Two Circles

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
                 .fontWeight(.black)
             
             Circle()
+            Circle()
         }
         .padding()
         .foregroundStyle(.green)
