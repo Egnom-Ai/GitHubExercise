3e84b44b325756af577d745df97d183b2483bdb0
Gustavo Monge <egnom.ai@gmail.com>
2024-08-29 08:51:51 -0600

    temp version 1.0

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "i.circle")
+            Image(systemName: "1.circle")
                 .resizable()
                 .scaledToFit()
 
